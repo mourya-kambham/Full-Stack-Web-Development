d5f59e0e8d7efbf3efc8e46f3102b92b6042f341
MOURYAK0901 <mourya80008@gmail.com>
2025-07-22 12:50:30 +0530

    Added code for request and response

--- Backend/express_dir/index.js
+++ Backend/express_dir/index.js
@@ -5,3 +5,8 @@ let port = 3000;
 app.listen(port,() => {
     console.log(`app is listening on port ${port}`);
 });
+
+app.use((req,res) => {
+    console.log("request received");
+    res.send(`This is a basic response and you are on localhost:${port}`)
+})
