9f3e881edca40d4b66b30ec092e9876ecb34cc00
Mourya <mourya80008@gmail.com>
2025-08-19 21:55:08 +0530

    index route

---
+++ Backend/REST_dir/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts =[
+    {
+        username: "Mourya",
+        content: "I am a Backend Developer"
+    },
+    {
+        username: "Chanakya",
+        content: "I am a Director"
+    },
+    {
+        username: "Shanmukh",
+        content: "I am a Businessman"
+    },
+];
+
+app.listen(port, () => {
+    console.log(`app is listening to port ${port}`);
+});
+
+app.get("/posts", (req,res) => {
+    res.render("rest.ejs", {posts});
+});
