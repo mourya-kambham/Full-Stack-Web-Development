741d531debd377118ede5f1afc9a3fb4686ed33a
Mourya <mourya80008@gmail.com>
2025-08-19 20:51:35 +0530

    Basic Instagram Template

--- Backend/instagram_ejs/index.js
+++ Backend/instagram_ejs/index.js
@@ -6,8 +6,19 @@ app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
 });
 
-app.get("/ig/:username", (req,res) => {
+/*app.get("/ig/:username", (req,res) => {
     const followers = ["Abhi", "Balu", "Chinnu", "Dhanush"];
     let {username} = req.params;
     res.render("instagram.ejs", {username, followers});
+});*/
+
+app.get("/ig/:username", (req,res) => {
+    let {username} = req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    if(data){
+        res.render("instagram.ejs", {data: instaData[username]});
+    }else{
+        res.render("error.ejs");
+    }
 });
