2dc42f857eaa97c5f26c7bcf37fa85aa053b7d0a
Mourya Kambham <mourya80008@gmail.com>
2026-02-10 15:48:31 +0530

    create collection

--- Mongo/index.js
+++ Mongo/index.js
@@ -9,3 +9,11 @@ main()
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
