649b4f93067e9efc6cf604799cf0abb995a68445
Mourya Kambham <mourya80008@gmail.com>
2026-02-10 16:34:34 +0530

    insertion

--- Mongo/index.js
+++ Mongo/index.js
@@ -17,3 +17,25 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+
+/*const user1 = new User({
+  name: "Adam",
+  email: "adam@gmail.com",
+  age: 43,
+});
+
+user1.save();
+
+const user2 = new User({
+  name: "Eve",
+  email: "eve@gmail.com",
+  age: 38,
+});
+
+user2.save();*/
+
+User.insertMany([
+  {name: "Tony", email: "tony@gmail.com", age: 51},
+  {name: "Peter", email: "peter@gmail.com", age: 22},
+  {name: "Bruce", email: "bruce@gmail.com", age: 47},
+]);
