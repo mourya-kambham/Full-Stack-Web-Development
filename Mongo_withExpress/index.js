98c82226935cacc9506eb6d30ca1edaf25dcec61
Mourya Kambham <mourya80008@gmail.com>
2026-03-25 23:14:05 +0530

    basic setup

---
+++ Mongo_withExpress/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+
+main()
+  .then(()=>{
+    console.log("connection successful");
+  })
+  .catch((err)=>console.log(err));
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/textapp");
+}
+
+app.set("views", path.join(__dirname, "views"));
+app.set("viewengine", "ejs");
+
+app.get("/", (req,res) => {
+    res.send("working root");
+});
+
+app.listen(8080, ()=>{
+    console.log("server is listening on port 8080");
+});
