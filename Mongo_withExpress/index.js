4c993888328cea3c11046b423dfe672bf20fb7e4
Mourya Kambham <mourya80008@gmail.com>
2026-03-26 23:30:55 +0530

    creating model

--- Mongo_withExpress/index.js
+++ Mongo_withExpress/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
 
 main()
   .then(()=>{
@@ -12,6 +13,21 @@ main()
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/textapp");
 }
+
+let chat1 = new Chat({
+  from: "neha",
+  to: "sneha",
+  msg: "send me",
+  created_at: new Date(),
+});
+
+chat1.save()
+  .then((res)=>{
+    console.log(res);
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("viewengine", "ejs");
