e5782801312f6256b5d31e02183e358837e4843a
Mourya Kambham <mourya80008@gmail.com>
2026-03-28 17:36:47 +0530

    create route

--- Mongo_withExpress/index.js
+++ Mongo_withExpress/index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
 
 main()
   .then(()=>{
@@ -39,6 +40,29 @@ app.set("viewengine", "ejs");
 app.get("/chats", async (req,res) => {
   let chats = await Chat.find();
   res.render("index.ejs", {chats});
+});
+
+app.get("/chats/new", (req,res) => {
+  res.render("new.ejs");
+});
+
+app.post("/chats", (req,res) => {
+  let {from, to, msg} = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    created_at: new Date(),
+  });
+  newChat
+    .save()
+    .then((res)=>{
+      console.log("Chat was saved");
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  res.redirect("/chats");
 });
 
 app.get("/", (req,res) => {
