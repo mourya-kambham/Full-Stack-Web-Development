b119e3f332ea0db616733bee59a5c64cfcd41237
Mourya Kambham <mourya80008@gmail.com>
2026-03-28 22:46:49 +0530

    edit & update route

--- Mongo_withExpress/index.js
+++ Mongo_withExpress/index.js
@@ -3,11 +3,13 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 main()
   .then(()=>{
@@ -62,6 +64,23 @@ app.post("/chats", (req,res) => {
     .catch((err)=>{
       console.log(err);
     });
+  res.redirect("/chats");
+});
+
+app.get("/chats/:id/edit", async (req,res) => {
+  let {id} = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", {chat});
+});
+
+app.put("/chats/:id", async (req, res) => {
+  let {id} = req.params;
+  let {msg: newMsg} = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    {msg: newMsg},
+    {runValidators: true, new: true}
+  );
   res.redirect("/chats");
 });
 
