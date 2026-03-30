d5744b9f400e85e6e8c5d4dbce3cffe81a94e22a
Mourya Kambham <mourya80008@gmail.com>
2026-03-31 00:16:30 +0530

    edit & update route

--- majorProject/app.js
+++ majorProject/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 main()
   .then(() => {
@@ -18,6 +19,7 @@ async function main(){
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req,res) => {
     res.send("Route is working");
@@ -42,6 +44,18 @@ app.post("/listings", async (req,res) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async (req,res) => {
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", {listing});
+});
+
+app.put("/listings/:id", async (req,res) => {
+  let {id} = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing});
+  res.redirect(`/listings/${id}`);
 });
 
 /*app.get("/testListing", async (req,res) => {
