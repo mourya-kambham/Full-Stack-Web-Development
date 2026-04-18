8b1c4da5408599fdcf7e381bb634cad17d02a874
Mourya Kambham <mourya80008@gmail.com>
2026-04-19 00:39:39 +0530

    joi

--- majorProject/app.js
+++ majorProject/app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 main()
   .then(() => {
@@ -51,10 +52,11 @@ app.get("/listings/:id", wrapAsync(async (req,res) => {
 
 //Create Route
 app.post("/listings", wrapAsync(async (req,res,next) => {
-    if(!req.body.listing){
-      throw new ExpressError(400,"Send valid data for listing");
-    }
-    const newListing = new Listing(req.body.listing);
+  let result = listingSchema.validate(req.body);
+  if(result.error){
+    throw new ExpressError(400, result.error);
+  }  
+  const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
 }));
