cbab1ed326cb93297b0301ea1b344de28015eec1
Mourya Kambham <mourya80008@gmail.com>
2026-04-19 22:22:08 +0530

    Deletion

--- majorProject/app.js
+++ majorProject/app.js
@@ -99,7 +99,7 @@ app.delete("/listings/:id", wrapAsync(async (req,res) => {
 }));
 
 //Reviews
-//Post route
+//Post Review route
 app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res) => {
   let listing = await Listing.findById(req.params.id);
   let newReview = new Review(req.body.review);
@@ -107,6 +107,14 @@ app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res) => {
   await newReview.save();
   await listing.save();
   res.redirect(`/listings/${listing._id}`);
+}));
+
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req,res) => {
+  let {id, reviewId} = req.params;
+  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/listings/${id}`);
 }));
 
 /*app.get("/testListing", async (req,res) => {
