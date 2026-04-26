c7a7e27fc0632c0bcc4554457908d75f076af9a0
Mourya Kambham <mourya80008@gmail.com>
2026-04-26 15:45:15 +0530

    authorization of reviews

--- majorProject/middleware.js
+++ majorProject/middleware.js
@@ -1,7 +1,8 @@
 const Listing = require("./models/listing");
 const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema, reviewSchema} = require("./schema.js");
-
+const review = require("./models/review.js");
+const Review = require("./models/review");
 
 module.exports.isLoggedIn = (req,res,next) => {
     if(!req.isAuthenticated()){
@@ -46,4 +47,14 @@ module.exports.validateReview = (req,res,next) => {
     }else{
         next();
     }
+};
+
+module.exports.isReviewAuthor = async(req,res,next) => {
+    let {id, reviewId} = req.params;
+    let review = await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)){
+        req.flash("error", "You are not the author of this review");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
 };
