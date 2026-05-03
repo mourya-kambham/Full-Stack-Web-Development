0d3156f865a0b943955131b2f8643e68d14ec34c
Mourya Kambham <mourya80008@gmail.com>
2026-05-03 12:00:09 +0530

    map marker & popup

--- majorProject/public/js/map.js
+++ majorProject/public/js/map.js
@@ -1,9 +1,21 @@
-maptilersdk.config.apiKey = 'bk3cQh8hEn5B03jOJki0';
+maptilersdk.config.apiKey = mapToken;
+
 const myMap = new maptilersdk.Map({
     container: 'map', // container's id or the HTML element to render the map
-    style:  maptilersdk.MapStyle.SATELLITE,
-    center: [77.209, 28.6139], // starting position [lng, lat]
-    zoom: 0.75, // starting zoom
+    style:  maptilersdk.MapStyle.STREETS,
+    center: listing.geometry.coordinates, // starting position [lng, lat]
+    zoom: 8, // starting zoom
 });
 
-
+myMap.on('load', () => {
+    new maptilersdk.Marker({color: "red"})
+        .setLngLat(listing.geometry.coordinates)
+        .setPopup(
+            new maptilersdk.Popup({offset: 25})
+                .setHTML(`
+                    <h4>${listing.location}</h4>
+                    <p>Exact location will be provided after booking</p>
+                `)
+        )
+        .addTo(myMap);
+});
